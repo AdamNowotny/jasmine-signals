4e1496db356bdada6babeaf3b043246513c4809d
Sam Maister <sam.maister@bbc.co.uk>
2014-10-09 13:08:13 +0100

    updated grunt and dependencies

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+	'use strict';
+
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+	grunt.registerTask('default', 'jasmine');
+
+	grunt.initConfig({
+		lint: {
+			files: [ '*.js' ]
+		},
+        jasmine: {
+            jasmineSignals: {
+                src: [ 'jasmine-signals.js', 'bower_components/js-signals/dist/signals.js' ],
+                options: {
+                    specs: 'jasmine-signalsSpec.js'
+                    //helpers: 'bower_components/es5-shim/es5-shim.min.js'
+                }
+            }
+        }
+	});
+};
